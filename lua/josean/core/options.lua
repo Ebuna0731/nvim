26c5ebee2219cda9979e97b8e78454378a356d6d
Ebuna0731 <berdenebuyan7@gmail.com>
2026-09-14 13:54:22 +0800

    Keep split proportions when opening windows

--- lua/josean/core/options.lua
+++ lua/josean/core/options.lua
@@ -33,3 +33,4 @@ opt.clipboard:append("unnamedplus")
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
+opt.equalalways = false
