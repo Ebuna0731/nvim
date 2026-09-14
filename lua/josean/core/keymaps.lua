4068f5eb85d9936cdff23410a141bb2606fe08c4
Ebuna0731 <berdenebuyan7@gmail.com>
2026-09-14 13:05:26 +0800

    Add terminal keymaps and work layout

--- lua/josean/core/keymaps.lua
+++ lua/josean/core/keymaps.lua
@@ -21,3 +21,18 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffe in new tab" })
+
+keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+keymap.set("n", "<leader>tt", "<cmd>botright split | term<CR>", { desc = "Open terminal below" })
+keymap.set("n", "<leader>tv", "<cmd>botright vsplit | term<CR>", { desc = "Open terminal on the right" })
+
+keymap.set("n", "<leader>tw", function()
+  vim.cmd("only")
+  vim.cmd("botright vsplit | term")
+  vim.cmd("vertical resize 80")
+  vim.cmd("wincmd h")
+  vim.cmd("split | term")
+  vim.cmd("resize 10")
+  vim.cmd("wincmd k")
+end, { desc = "Work layout: code + terminal + Claude" })
--- lua/josean/plugins/treesitter.lua
+++ lua/josean/plugins/treesitter.lua
@@ -1,54 +1,91 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  event = { "BufReadPre", "BufNewFile" },
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
     "windwp/nvim-ts-autotag",
   },
   config = function()
-    local treesitter = require("nvim-treesitter.configs")
+    local treesitter = require("nvim-treesitter")
 
-    treesitter.setup({
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
+    treesitter.setup()
+
+    treesitter.install({
+      "json",
+      "javascript",
+      "typescript",
+      "tsx",
+      "yaml",
+      "html",
+      "css",
+      "prisma",
+      "markdown",
+      "markdown_inline",
+      "svelte",
+      "graphql",
+      "bash",
+      "lua",
+      "vim",
+      "dockerfile",
+      "gitignore",
+      "query",
+      "vimdoc",
+      "c",
+    })
+
+    vim.treesitter.language.register("bash", "zsh")
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("josean-treesitter", { clear = true }),
+      callback = function(args)
+        local buf = args.buf
+        local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
+        if not lang then
+          return
+        end
+        if not pcall(vim.treesitter.language.add, lang) then
+          return
+        end
+        pcall(vim.treesitter.start, buf, lang)
+        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
 
     require("nvim-ts-autotag").setup()
 
-    vim.treesitter.language.register("bash", "zsh")
+    local selection = {}
+
+    local function select_range(node)
+      local sr, sc, er, ec = node:range()
+      vim.api.nvim_win_set_cursor(0, { sr + 1, sc })
+      vim.cmd("normal! v")
+      vim.api.nvim_win_set_cursor(0, { er + 1, math.max(ec - 1, 0) })
+    end
+
+    vim.keymap.set({ "n", "x" }, "<C-space>", function()
+      local node
+      if vim.fn.mode() ~= "v" or #selection == 0 then
+        selection = {}
+        node = vim.treesitter.get_node()
+      else
+        node = selection[#selection]:parent()
+      end
+      if not node then
+        return
+      end
+      selection[#selection + 1] = node
+      vim.cmd("normal! \27")
+      select_range(node)
+    end, { desc = "Increment selection" })
+
+    vim.keymap.set("x", "<BS>", function()
+      if #selection < 2 then
+        return
+      end
+      table.remove(selection)
+      vim.cmd("normal! \27")
+      select_range(selection[#selection])
+    end, { desc = "Decrement selection" })
   end,
 }
